350cba87333974725874c9f8d5843029b811100c
André Lopes <andreglopes2005@gmail.com>
2024-07-29 18:58:54 +0100

    publish at the end if it has been changed

--- src/mission_controller.cpp
+++ src/mission_controller.cpp
@@ -5,7 +5,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int8.hpp"
 #include "std_msgs/msg/int32.hpp"
-#include "std_msgs/msg/bool.hpp"
 #include "lart_msgs/msg/mission.hpp"
 #include "lart_msgs/msg/state.hpp"
 
@@ -15,17 +14,8 @@
 #define LAPS_EBS_TEST 1 // if needed, not implemented for now
 #define LAPS_AUTOCROSS 1 // according to D6.4.2 from rule book
 
-enum critical_as_mission {
-    ACCELERATION=1,
-    SKIDPAD=2,
-    TRACKDRIVE=3,
-    EBS_TEST=4,
-    INSPECTION=5,
-    AUTOCROSS=6
-    //UNKNOWN_MISSION
-};
-
 using std::placeholders::_1;
+void check_laps(int laps);
 
 class Mission_controller : public rclcpp::Node
 {
@@ -35,13 +25,14 @@ public:
     lap_subscriber_ = this->create_subscription<std_msgs::msg::Int32>("LapCount", 5, std::bind(&Mission_controller::lap_count, this, _1));
     subscription_ = this->create_subscription<std_msgs::msg::Int8>("mission_button", 10, std::bind(&Mission_controller::process_mission, this, _1));//ask the correct name of the topic
     mission_pub_ = this->create_publisher<lart_msgs::msg::Mission>("mission", 10);
-    mission_finished_pub_ = this->create_publisher<std_msgs::msg::Bool>("mission_finished", 10);//publisher to state_controller true if all laps were made
-    mission_finished.data=false;
+    mission_finished_pub_ = this->create_publisher<std_msgs::msg::Int8>("mission_finished", 10);//publisher to state_controller true if all laps were made
+    mission_finished.data=0;//1 if true
   }
 
 private:
-  lart_msgs::msg::Mission mission_msg;
-  std_msgs::msg::Bool mission_finished;
+  lart_msgs::msg::Mission current_mission_msg;
+  lart_msgs::msg::Mission previous_mission_msg;
+  std_msgs::msg::Int8 mission_finished;
   int32_t lap_counter;
 
   void lap_count(const std_msgs::msg::Int32 & msg) 
@@ -54,66 +45,61 @@ private:
     int mission = msg.data;
 
     switch(mission){
-      case ACCELERATION:
-        mission_msg.data= lart_msgs::msg::Mission::ACCELERATION;
+      case lart_msgs::msg::Mission::ACCELERATION:
+        current_mission_msg.data= lart_msgs::msg::Mission::ACCELERATION;
         RCLCPP_INFO(this->get_logger(), "Mission is acceleration('%d')", mission);
-        mission_pub_->publish(mission_msg);
-        if (lap_counter >= LAPS_ACCELERATION){
-          mission_finished.data = true;
-          mission_finished_pub_->publish(mission_finished);
-        }
+        check_laps(LAPS_ACCELERATION);
         break;
 
-      case SKIDPAD:
-        mission_msg.data= lart_msgs::msg::Mission::SKIDPAD;
+      case lart_msgs::msg::Mission::SKIDPAD:
+        current_mission_msg.data= lart_msgs::msg::Mission::SKIDPAD;
         RCLCPP_INFO(this->get_logger(), "Mission is skidpad('%d')", mission);
-        mission_pub_->publish(mission_msg);
-        if (lap_counter >= LAPS_SKIDPAD){
-          mission_finished.data = true;
-          mission_finished_pub_->publish(mission_finished);
-        }
+        check_laps(LAPS_SKIDPAD);
         break;
 
-      case TRACKDRIVE:
-        mission_msg.data= lart_msgs::msg::Mission::TRACKDRIVE;
+      case lart_msgs::msg::Mission::TRACKDRIVE:
+        current_mission_msg.data= lart_msgs::msg::Mission::TRACKDRIVE;
         RCLCPP_INFO(this->get_logger(), "Mission is trackDrive('%d')", mission);
-        mission_pub_->publish(mission_msg);
-        if (lap_counter >= LAPS_TRACKDRIVE){
-          mission_finished.data = true;
-          mission_finished_pub_->publish(mission_finished);
-        }
+        check_laps(LAPS_TRACKDRIVE);
         break;
 
-      case EBS_TEST:
-        mission_msg.data= lart_msgs::msg::Mission::EBS_TEST;
+      case lart_msgs::msg::Mission::EBS_TEST:
+        current_mission_msg.data= lart_msgs::msg::Mission::EBS_TEST;
         RCLCPP_INFO(this->get_logger(), "Mission is ebs test('%d')", mission);
-        mission_pub_->publish(mission_msg);
         break;
 
-      case INSPECTION:
-        mission_msg.data= lart_msgs::msg::Mission::INSPECTION;
+      case lart_msgs::msg::Mission::INSPECTION:
+        current_mission_msg.data= lart_msgs::msg::Mission::INSPECTION;
         RCLCPP_INFO(this->get_logger(), "Mission is inspection('%d')", mission);
-        mission_pub_->publish(mission_msg);
         break;
 
-      case AUTOCROSS:
-        mission_msg.data= lart_msgs::msg::Mission::AUTOCROSS;
+      case lart_msgs::msg::Mission::AUTOCROSS:
+        current_mission_msg.data= lart_msgs::msg::Mission::AUTOCROSS;
         RCLCPP_INFO(this->get_logger(), "Mission is autocross('%d')", mission);
-        mission_pub_->publish(mission_msg);
-        if (lap_counter >= LAPS_AUTOCROSS){
-          mission_finished.data = true;
-          mission_finished_pub_->publish(mission_finished);
-        }
+        check_laps(LAPS_AUTOCROSS);
         break;
 
       default:
         RCLCPP_INFO(this->get_logger(), "Unknown mission('%d')", mission);
     }
+    
+    if(current_mission_msg.data != previous_mission_msg.data){
+      previous_mission_msg.data = current_mission_msg.data;
+      mission_pub_->publish(current_mission_msg);
+    }
   }
+
+  void check_laps(int laps){
+    if(lap_counter >= laps){
+      mission_finished.data = 1;
+      mission_finished_pub_->publish(mission_finished);
+    }
+  }
+
   rclcpp::Subscription<std_msgs::msg::Int8>::SharedPtr subscription_;
   rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr lap_subscriber_;
   rclcpp::Publisher<lart_msgs::msg::Mission>::SharedPtr mission_pub_;
-  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr mission_finished_pub_;
+  rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr mission_finished_pub_;
 };
 
 
